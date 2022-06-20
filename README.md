0a0cde86656cc38eb4e513e65ecb5df4ba51d656
Alessio Saltarin <alessiosaltarin@gmail.com>
2022-06-20 14:21:57 +0200

    New Deno and ViewEngine versions

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ A simple template App Server written in TypeScript for Deno.
 
 It uses:
 
-* [Deno View Engine](https://deno.land/x/view_engine@v1.5.0)
+* [Deno View Engine](https://deno.land/x/view_engine@v1.5.1)
 * [Denjucks Templating Language](https://deno.land/x/denjucks@1.1.1)
 * [Bootstrap Responsive HTML Library](https://getbootstrap.com/)
 * [DyeLog Logger](https://deno.land/x/dyelog@v0.1.1)
--- src/deps.ts
+++ src/deps.ts
@@ -5,10 +5,11 @@
  * MIT License
  */
 
+
 export { Application, Router, Status, send } from "https://deno.land/x/oak@v10.5.1/mod.ts";
-export { cyan, gray, red, yellow } from "https://deno.land/std@0.134.0/fmt/colors.ts";
-export { sprintf } from "https://deno.land/std@0.108.0/fmt/printf.ts";
+export { cyan, gray, red, yellow } from "https://deno.land/std@0.144.0/fmt/colors.ts";
+export { sprintf } from "https://deno.land/std@0.144.0/fmt/printf.ts";
 export { DyeLog, LogLevel } from "https://deno.land/x/dyelog@v0.1.3/mod.ts";
-export { oakAdapter, denjuckEngine, viewEngine } from "https://deno.land/x/view_engine@v10.5.1b/mod.ts";
+export { oakAdapter, denjuckEngine, viewEngine } from "https://deno.land/x/view_engine@v10.5.1d/mod.ts";
 
 export type { RouterContext } from "https://deno.land/x/oak@v10.5.1/mod.ts";
