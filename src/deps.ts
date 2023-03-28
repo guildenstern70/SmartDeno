dbfff25a006d04bf6ed4bcd4027b72e6eb4b63b2
Alessio Saltarin <alessiosaltarin@gmail.com>
2023-03-28 12:20:48 +0200

    Libraries upgrade

--- src/deps.ts
+++ src/deps.ts
@@ -7,19 +7,18 @@
  *
  */
 
-
 // Deno Standard Library
-export { cyan, gray, red, yellow } from "https://deno.land/std@0.159.0/fmt/colors.ts";
-export { sprintf } from "https://deno.land/std@0.159.0/fmt/printf.ts";
+export { cyan, gray, red, yellow } from "https://deno.land/std@0.181.0/fmt/colors.ts";
+export { sprintf } from "https://deno.land/std@0.181.0/fmt/printf.ts";
 
-// Oak
-export { Application, Router, Status, send } from "https://deno.land/x/oak@v11.1.0/mod.ts";
-export type { RouterContext } from "https://deno.land/x/oak@v10.6.0/mod.ts";
-export { Session } from "https://deno.land/x/oak_sessions@v4.0.5/mod.ts";
+// Oak: Middleware Framework - https://deno.land/x/oak@v12.1.0
+export { Application, Router, Status, send } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+export type { RouterContext } from "https://deno.land/x/oak@v12.1.0/mod.ts";
+export { Session } from "https://deno.land/x/oak_sessions@v4.1.0/mod.ts";
 
-// DyeLog
+// DyeLog: Colorful logger - https://deno.land/x/dyelog@v0.1.3
 export { DyeLog, LogLevel } from "https://deno.land/x/dyelog@v0.1.3/mod.ts";
 
-// Eta
-export { render } from "https://deno.land/x/eta@v1.12.3/mod.ts";
+// Eta: Templating Engine - https://deno.land/x/eta@v2.0.1
+export { render } from "https://deno.land/x/eta@v2.0.1/mod.ts";
 
